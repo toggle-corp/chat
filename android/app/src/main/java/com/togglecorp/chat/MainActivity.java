aad7a250b66c622c4f94eb7b2add8eef993c0b9e
Frozen Helium <fren.ankit@gmail.com>
2016-10-19 14:03:03 +0545

    Add some views

--- android/app/src/main/java/com/togglecorp/chat/MainActivity.java
+++ android/app/src/main/java/com/togglecorp/chat/MainActivity.java
@@ -1,15 +1,33 @@
 package com.togglecorp.chat;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private User mUser;
     private static FirebaseDatabase mFDb;
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +47,54 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
+
+        // Initialize the nav drawer
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Set the nav drawer header
+        View header = mNavigationView.getHeaderView(0);
+        ((TextView)header.findViewById(R.id.username)).setText(mUser.getUser().getDisplayName());
+        ((TextView)header.findViewById(R.id.extra)).setText(mUser.getUser().getEmail());
+        Picasso.with(this)
+                .load(mUser.getUser().getPhotoUrl())
+                .into((CircleImageView)header.findViewById(R.id.avatar));
+
+        // Initialize the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        // The hamburger icon
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        };
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+
+    private NavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            mDrawerLayout.closeDrawers();
+            return true;
+        }
+    };
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
